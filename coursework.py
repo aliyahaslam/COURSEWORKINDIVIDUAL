4e6f2273cb8d17dc97863370b41f316406ce82ae
aliyahaslam <152289844+aliyahaslam@users.noreply.github.com>
2024-05-06 01:28:43 +0100

    Add files via upload
    updated

--- coursework.py
+++ coursework.py
@@ -1,2 +1,7 @@
 import streamlit as st 
-st.title("BIODIVERSITY INSIGHTS DASHBOARD") 
+st.title("Biodiversity Insights Dashboard")
+st.subheader("Exploring the key insights found from our group project")
+st.subheader("navigation ")
+import pandas as pd
+df = pd.read_excel("c:/Users/aliya/OneDrive/Desktop/Data Science Project Lifecycle Coursework/individual courseowrk/4Lenses.xlsx")
+st.write(df)
